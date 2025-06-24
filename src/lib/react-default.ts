1b9850a45090d20fd6a97c9d3d512e1786cd498f
jake0lawrence <43353705+jake0lawrence@users.noreply.github.com>
2025-06-23 19:49:41 -0500

    Update react-default.ts

--- src/lib/react-default.ts
+++ src/lib/react-default.ts
@@ -1,4 +1,3 @@
-# src/lib/react-default.ts
 import * as ReactNS from 'react';
-export default ReactNS;      // default export
-export * from 'react';       // keep named exports working
+export default ReactNS;
+export * from 'react';
