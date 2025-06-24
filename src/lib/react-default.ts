4e5b8614f551deeea90e91c56fb79659693eaa21
jake0lawrence <43353705+jake0lawrence@users.noreply.github.com>
2025-06-23 22:01:01 -0500

    Delete src/lib/react-default.ts

--- src/lib/react-default.ts
+++
@@ -1,5 +0,0 @@
-import * as ReactNS from 'react';
-// Patch the module once so MDX's default import works
-// @ts-expect-error – we knowingly add the field
-(ReactNS as any).default = ReactNS;
-export default ReactNS;
